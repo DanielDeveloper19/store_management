827870374a429bfd66bf0a83591f7e1d52ca29ca
HARD-CODE daniel_camilo <daniel256531@gmail.com>
2026-04-17 22:57:32 -0500

    Base Integration and Unit tests ready

--- src/test/java/com/finaltodocode/final_todocode/FinalTodocodeApplicationTests.java
+++ src/test/java/com/finaltodocode/final_todocode/FinalTodocodeApplicationTests.java
@@ -1,13 +1,16 @@
 package com.finaltodocode.final_todocode;
 
+import com.finaltodocode.final_todocode.integrationTestsConfig.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FinalTodocodeApplicationTests {
+class FinalTodocodeApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
+        // This will now succeed because BaseIntegrationTest
+        // provides the Docker DB connection
 	}
 
 }
